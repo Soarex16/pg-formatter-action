5c3fd27c274030b541be33a42001e4d92ff60423
soarex <soarex16@gmail.com>
2021-11-04 01:36:09 +0300

    add some new test files

---
+++ test_files/ex1.sql
@@ -0,0 +1,12 @@
+select a,b,c
+from tablea
+            join tableb on ( tablea.a=tableb.a)
+left outer  join tabled on ( tabled.a=tableb.a)
+left join
+
+    tablee on ( tabled.a=tableb.a)
+where tablea.x = 1 and tableb.y=1
+group by tablea.a, tablec.c
+order by tablea.a, tablec.c;
+
+
