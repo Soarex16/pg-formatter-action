af4307d51660335583bb22a60c4b33111d0b5f32
Soarex16 <Soarex16@users.noreply.github.com>
2021-11-03 22:40:14 +0000

    Apply formatting

--- test_files/ex0.sql
+++ test_files/ex0.sql
@@ -1,11 +1,20 @@
-select a,b,c
-from tablea
-join tableb on ( tablea.a=tableb.a)
-join tablec on ( tablec.a=tableb.a)
-left outer join tabled on ( tabled.a=tableb.a)
-left join tablee on ( tabled.a=tableb.a)
-where tablea.x = 1 and tableb.y=1
-group by tablea.a, tablec.c
-order by tablea.a, tablec.c;
-
+SELECT
+    a,
+    b,
+    c
+FROM
+    tablea
+    JOIN tableb ON (tablea.a = tableb.a)
+    JOIN tablec ON (tablec.a = tableb.a)
+    LEFT OUTER JOIN tabled ON (tabled.a = tableb.a)
+    LEFT JOIN tablee ON (tabled.a = tableb.a)
+WHERE
+    tablea.x = 1
+    AND tableb.y = 1
+GROUP BY
+    tablea.a,
+    tablec.c
+ORDER BY
+    tablea.a,
+    tablec.c;
 
